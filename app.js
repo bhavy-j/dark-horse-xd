88a2a113366e55a516c4801065962b86004000bd
Bhavy Jogani <bhavyjogani123@gmail.com>
2025-12-11 12:29:54 +0530

    gradient added

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 window.addEventListener('load', () => {
-    console.log('Window is loaded');
-
     const msgElement = document.getElementById('dynamic-msg');
     if (msgElement) {
         msgElement.textContent = 'Hello From js-logic branch!!';
+    }
+
+    const button = document.getElementById('change-experience');
+    if (button) {
+        button.addEventListener('click', () => {
+            //change bg color to random gradient
+            const randomColor1 = Math.floor(Math.random() * 256);
+            const randomColor2 = Math.floor(Math.random() * 256);
+            const randomColor3 = Math.floor(Math.random() * 256);
+            document.body.style.background = `linear-gradient(to right, rgb(${randomColor1}, ${randomColor2}, ${randomColor3}), rgb(${randomColor3}, ${randomColor1}, ${randomColor2}))`;
+        });
     }
 });
